ecd3a5132c7c8ceaa19a378d7eefee3086857423
mdsa <mdsa@funzio.com>
2011-11-29 21:25:56 -0800

    Initial Commit

---
+++ MicBlow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MicBlow
+//
+//  Created by Michel D'Sa on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MicBlow/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  MicBlow
+//
+//  Created by Michel D'Sa on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
